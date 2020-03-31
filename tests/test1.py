fa49e615ea7d36403b7e19890bb20564ea8cfaff
AimeeLiu01 <978611061@qq.com>
2020-03-31 11:39:32 +0800

    Initial commit

---
+++ tests/test1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liuchang
+@software: PyCharm
+@file: test1.py.py
+@time: 2020-03-31 11:33
+'''
+import os
+import sys
+import re
+import json
+import datetime
+
+dir_mytest = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, dir_mytest)
+
